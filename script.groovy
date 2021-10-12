d6acfa760dfb80811bec9bbad3569ad70950614d
Karthik <karthik.krishnaswamy17@gmail.com>
2021-10-12 16:13:47 +0530

    Updates

---
+++ script.groovy
@@ -0,0 +1,23 @@
+#!/bin/bash/env groovy
+def buildJar(){
+	echo "Building Jar...."
+	sh 'mvn package'
+
+}
+
+def buildImage(){
+		script{
+                echo "Building Image"
+                withCredentials([usernamePassword(credentialsId:'docker-hub-account',usernameVariable:'user',passwordVariable:'pass')]){
+                sh 'docker build -t karthik0517/java-maven-app:${BUILD_NUMBER} .'
+                sh "echo $pass |docker login --username $user --password-stdin"
+                sh 'docker push karthik0517/java-maven-app:${BUILD_NUMBER}'
+                }
+              }
+}
+
+def deployApp(){
+	echo "Deploying...groovy"
+}
+
+return this
